90d13fa19ec19305af2b120894d19d2cb1bb45cc
CherpakAndrii <andriycherpak.education@gmail.com>
2021-04-18 12:19:33 +0300

    init repos

---
+++ DLL.cs
@@ -0,0 +1,122 @@
+﻿namespace ADS_4
+{
+    public unsafe class DLL
+    {
+	    private int _size = 0;
+	    public Node* First;
+	    public Node* Last;
+
+	    public struct Node
+	    {
+		    public double Value;
+		    public Node* Prev;
+		    public Node* Next;
+
+		    public Node(double val)
+		    {
+			    Value = val;
+			    Prev = Next = null;
+		    }
+	    }; 
+	    public DLL() 
+        {
+			First = Last = null;
+		}
+	    public void PushBack(double val)
+		{
+			Node n = new Node(val);
+			_size++;
+			if (First == null)
+			{
+				First = Last = &n;
+			}
+			else
+			{
+				Last->Next = &n;
+				n.Prev = Last;
+				Last = &n;
+			}
+		}
+
+	    public void PushFront(double val)
+		{
+			Node n = new Node(val);
+			_size++;
+			if (First == null)
+			{
+				First = Last = &n;
+			}
+			else
+			{
+				First->Prev = &n;
+				n.Next = First;
+				First = &n;
+			}
+		}
+
+	    public double Delete(int ind)
+		{
+			Node* n = First;
+			if (ind < 0 && -ind <= _size) ind = _size - ind;
+			if (ind > _size) return Last->Value;
+			for (int ctr = 0; ctr < ind; ctr++) n = n->Next;
+			n->Prev->Next = n->Next; 
+			n->Next->Prev = n->Prev;
+			_size--; 
+			return n->Value;
+		}
+
+	    public void Insert(int ind, double val)
+		{
+			Node n = new Node(val);
+			Node* pos = First;
+			if (ind < 0 && -ind <= _size) ind = _size - ind; 
+			if (ind <= 0) PushFront(val);
+			else if (ind>= _size) PushBack(val);
+			else
+			{ 
+				for (int ctr = 1; ctr < ind; ctr++) pos = pos->Next;
+				n.Next = pos->Next; 
+				n.Next->Prev = &n; 
+				n.Prev = pos; 
+				pos->Next = &n; 
+				_size++; 
+			} 
+		}
+
+	    public double PopFront()
+		{
+			Node* n = First;
+			First->Next->Prev = null;
+			First = First->Next;
+			_size--;
+			return n->Value;
+		}
+
+	    public double PopBack()
+		{
+			Node* n = Last;
+			Last->Prev->Next = null;
+			Last = Last->Prev;
+			_size--;
+			return n->Value;
+		}
+
+	    public double this[int ind]
+		{
+			get
+			{
+				Node* n = First;
+				if (ind < 0 && -ind <= _size) ind = _size - ind;
+				if (ind > _size) return Last->Value;
+				for (int ctr = 0; ctr < ind; ctr++) n = n->Next;
+				return n->Value;
+			}
+		}
+
+	    public int get_size()
+	    {
+		    return _size;
+	    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace ADS_4
+{
+    class Program
+    {
+        static unsafe void Main()
+        {
+            DLL lst = GetList();
+            DLL newLst = GetNewList(lst);
+            ListToFile(newLst);
+        }
+
+        static void GenerateFile()
+        {
+            Console.Write("Enter the amount of numbers: ");
+            int n = Int32.Parse(Console.ReadLine());
+            using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + @"\txt.txt", false, Encoding.Default))
+            {
+                sw.WriteLine(n);
+                Random rand = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    sw.Write($"{rand.NextDouble()*50:N3} ");
+                }
+                sw.WriteLine();
+            }
+        }
+
+        static unsafe DLL GetList()
+        {
+            DLL lst = new DLL();
+            using (StreamReader sr = new StreamReader(Environment.CurrentDirectory + @"\txt.txt", System.Text.Encoding.Default))
+            {
+                string[] slices = sr.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in slices) lst.PushBack(Double.Parse(s));
+            }
+            return lst; 
+        }
+
+        static unsafe DLL GetNewList(DLL inpList)
+        {
+            DLL.Node* item = inpList.First;
+            double lastValue = inpList.Last->Value;
+            DLL nLst = new DLL();
+            for (int i = 0; i < inpList.get_size() - 2; i++)
+            {
+                nLst.PushBack(item->Value-lastValue);
+                item = item->Next;
+            }
+            nLst.PushBack(item->Value-lastValue);
+            return nLst;
+        }
+        static unsafe void ListToFile(DLL newList)
+        {
+            DLL.Node* node = newList.First;
+            using (StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + @"\new.txt", false, Encoding.Default))
+            {
+                Console.WriteLine(newList.get_size());
+                for (int i = 0; i < newList.get_size(); i++)
+                {
+                    sw.Write($"{node->Value} ");
+                    Console.Write($"{node->Value} ");
+                    node = node->Next;
+                }
+            }
+        } 
+    }
+}
